1abb5891bc9b1e9c53308efa3352c0d5dcb82b2b
MeatBoyUSM <andres.huerta.14@sansano.usm.cl>
2016-12-01 20:21:17 -0300

    Add files via upload

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,53 +1,70 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <semaphore.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
-#include  <sys/types.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <ctype.h>
-#include <semaphore.h>
-#include <pthread.h>
+#include <fcntl.h>
+
+FILE *fp;
 
 int main(){
-    sem_t *mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    sem_t *escritor = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    int *en_espera = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    sem_init(mutex, 0, 1);
-    sem_init(escritor, 0, 1);
-    int writer = fork();
+   // sem_t *mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+   // sem_t *lectores = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    //sem_init(mutex, 0, 1);
+   // sem_init(lectores, 0, 1);
+    sem_t *mutex = sem_open("pmutex", O_CREAT | O_EXCL, 0644, 1);
+    sem_unlink("pmutex");
+
+    sem_t *lectores = sem_open("plectores", O_CREAT | O_EXCL, 0644, 1);
+    sem_unlink("plectores");
+
+    char text[200];
+    int lector = fork();
     int i = 0;
-    if(writer == 0){
+
+    if(lector == 0){ //Lector
         while (i < 3){
-            printf("HIJO\n");
-            sem_wait(escritor);
-            if ((*en_espera) == 0){
-                sem_wait(mutex);
-                getchar();
-                printf("HIJO QLIAO\n");
-                sem_post(mutex);
-            }
-            sem_post(escritor);
-            i++;
-        }
-    }
-    else{
-        while (i < 3){
-            sem_wait(escritor);
-            (*en_espera)++;
+            sem_wait(lectores);
             sem_wait(mutex);
-            getchar();
-            printf("SOY EL PADRE\n");
+            //zona critica
+            printf("Lectores Leyendo\n");
+            fp = fopen("texto.txt", "r");
+            while((fgets(text, sizeof(text), fp) != NULL)){
+                printf("%s", text);
+            }
+            fclose(fp);
+            printf("\n");
             sem_post(mutex);
-            sem_post(escritor);
+            sem_post(lectores);
             i++;
         }
     }
 
-    munmap(mutex, sizeof(sem_t));
-    munmap(escritor, sizeof(sem_t));
-    munmap(en_espera, sizeof(int));
+    else{   //Escritor
+        while(i < 3){
+            sem_wait(mutex);
+            sem_wait(lectores);
+            if(i == 0){
+                printf("Escritor Escribe\n");
+            }
+            fp = fopen("texto.txt", "a");
+            printf("Poema: ");
+            fgets(text, sizeof(text), stdin);
+            fputs(text, fp);
+            fclose(fp);
+            sem_post(lectores);
+            sem_post(mutex);
+            i++;
+        }
+    }
+    if(lector != 0){
+        wait(NULL);
+    }
+
     return 0;
 }
